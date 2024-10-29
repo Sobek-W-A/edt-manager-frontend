6f67b2c074298703375a1592f1d9c924534abcaa
Maxime Marcelin <maisonmarc7@gmail.com>
2024-10-29 04:22:56 +0100

    Added basic get methods.

---
+++ app/src/scripts/API/APITypes/Users.ts
@@ -0,0 +1,7 @@
+export type UserType = {
+    id: number;
+    login: string;
+    firstname: string;
+    lastname: string;
+    mail: string;
+}
--- app/src/scripts/API/ModelAPIs/UserAPI.ts
+++ app/src/scripts/API/ModelAPIs/UserAPI.ts
@@ -1,3 +1,8 @@
+import {api} from "../API.ts";
+import {HTTPMethod} from "../Enums/HTTPMethod.ts";
+import APIResponse from "../Responses/APIResponse.ts";
+import {UserType} from "../APITypes/Users.ts";
+
 /**
  * API methods for user endpoints.
  */
@@ -5,4 +10,42 @@ export default class UserAPI {
 
     static BASE_USER_URL: string = '/user';
 
+    /**
+     * This method sends the request to get personal information about the connected user.
+     * @returns A promise that is either a User or an error.
+     */
+    static getCurrentUser(): Promise<APIResponse<UserType>> {
+        return api.requestLogged<UserType>(
+            HTTPMethod.GET,
+            UserAPI.BASE_USER_URL + "/me",
+            undefined,
+            undefined
+        );
+    }
+
+    /**
+     * This method sends the request to get personal information about the users registered in the API.
+     * @returns A promise that is either an array of User or an error.
+     */
+    static getAllUsers(): Promise<APIResponse<[UserType]>> {
+        return api.requestLogged<[UserType]>(
+            HTTPMethod.GET,
+            UserAPI.BASE_USER_URL + "/all",
+            undefined,
+            undefined
+        );
+    }
+
+    /**
+     * This method sends the request to get personal information about the users registered in the API.
+     * @returns A promise that is either an array of User or an error.
+     */
+    static getUserByID(user_id: number): Promise<APIResponse<UserType>> {
+        return api.requestLogged<UserType>(
+            HTTPMethod.GET,
+            UserAPI.BASE_USER_URL + `/${user_id}`,
+            undefined,
+            undefined
+        );
+    }
 }
