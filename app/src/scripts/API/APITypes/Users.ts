70436261193c2023ec2b486c55fa53146468dc98
Maxime Marcelin <maisonmarc7@gmail.com>
2024-10-29 05:18:59 +0100

    Added the modify user request and model handler.

--- app/src/scripts/API/APITypes/Users.ts
+++ app/src/scripts/API/APITypes/Users.ts
@@ -1,7 +1,16 @@
 export type UserType = {
-    id: number;
-    login: string;
-    firstname: string;
-    lastname: string;
-    mail: string;
+    id:         number;
+    login:      string;
+    firstname:  string;
+    lastname:   string;
+    mail:       string;
+}
+
+export type UserInPatchType = {
+    login:                  string | undefined;
+    firstname:              string | undefined;
+    lastname:               string | undefined;
+    mail:                   string | undefined;
+    password:               string | undefined;
+    password_confirmation:  string | undefined;
 }
--- app/src/scripts/API/ModelAPIs/UserAPI.ts
+++ app/src/scripts/API/ModelAPIs/UserAPI.ts
@@ -1,7 +1,7 @@
 import {api} from "../API.ts";
 import {HTTPMethod} from "../Enums/HTTPMethod.ts";
 import APIResponse from "../Responses/APIResponse.ts";
-import {UserType} from "../APITypes/Users.ts";
+import {UserInPatchType, UserType} from "../APITypes/Users.ts";
 
 /**
  * API methods for user endpoints.
@@ -18,20 +18,6 @@ export default class UserAPI {
         return api.requestLogged<UserType>(
             HTTPMethod.GET,
             UserAPI.BASE_USER_URL + "/me",
-            undefined,
-            undefined
-        );
-    }
-
-    /**
-     * This method sends the request to get personal information about the users registered in the API.
-     * @returns A promise that is either an array of User or an error.
-     */
-    static getAllUsers(): Promise<APIResponse<[UserType]>> {
-        return api.requestLogged<[UserType]>(
-            HTTPMethod.GET,
-            UserAPI.BASE_USER_URL + "/all",
-            undefined,
             undefined
         );
     }
@@ -44,8 +30,32 @@ export default class UserAPI {
         return api.requestLogged<UserType>(
             HTTPMethod.GET,
             UserAPI.BASE_USER_URL + `/${user_id}`,
-            undefined,
             undefined
+        );
+    }
+
+    /**
+     * This method sends the request to get personal information about the users registered in the API.
+     * @returns A promise that is either an array of User or an error.
+     */
+    static getAllUsers(): Promise<APIResponse<UserType[]>> {
+        return api.requestLogged<UserType[]>(
+            HTTPMethod.GET,
+            UserAPI.BASE_USER_URL + "/all",
+            undefined
+        );
+    }
+
+    /**
+     * This method updates the user qualified by the id provided using the information of the body.
+     * @param user_id User id to qualify the user to modify.
+     * @param body    Body containing the new information of the user.
+     */
+    static updateUser(user_id: number, body: UserInPatchType): Promise<APIResponse<undefined>> {
+        return api.requestLogged<undefined>(
+            HTTPMethod.PATCH,
+            UserAPI.BASE_USER_URL + `/${user_id}`,
+            JSON.stringify(body)
         );
     }
 }
