eee17c52c62873e51d754a3214c2b05e89d30bfd
MatthieuGalante <matthieu.galante@gmail.com>
2024-11-19 01:15:44 +0100

    - réparation de la navbar cassée par je sais pas qui qui a mis un grand coup de chat gpt alors qu'elle marchait très bien. - refonte d'un peu de css pour avoir un site plus beau - mise en place du squelette de la vue qui va permettre l'affectation des professeurs aux cours. - mise en place des composants associés à la nouvelle vue avec des TODO pour chacun pour qu'on travail bien tous bien sur nos petits composants.

---
+++ app/src/Components/CoursesManager/Tree.tsx
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class Tree extends Component {
+    render() {
+        return (
+            <div>
+                <p>TODO JULIEN</p>
+            </div>
+        );
+    }
+}
+
+export default Tree;
--- app/src/Components/Utils/Navbar.tsx
+++ app/src/Components/Utils/Navbar.tsx
@@ -3,32 +3,37 @@ import '../../index.css';
 
 function Navbar() {
     return (
-        <div className="w-full bg-green-700 mb-12 border-b-2 border-t-2 p-2 pb-3 text-xl font-bold text-white grid grid-cols-10">
-            <div className="col-start-1 col-span-2 flex items-center">
+        <div className="w-full bg-green-700 border-b-2 border-t-2 p-2 pb-3 text-xl font-bold text-white grid grid-cols-10">
+            <div className="col-start-1 col-span-1 flex items-center">
                 <Link to="/">
                     <span className="ml-2">SOBEK W.A.</span>
                 </Link>
             </div>
-
-            {/* Navbar Links */}
-            <div className="col-start-3 col-span-6 flex justify-center space-x-4 items-center">
-                <Link to="/example" className="text-sm hover:text-green-300 transition duration-200">
-                    EXAMPLE
-                </Link>
-                {/* Ajouter d'autres liens ici si nécessaire */}
-            </div>
-
             {/* Login Link */}
-            <div className="col-start-10 col-span-1 flex justify-end items-center">
+            <div className="col-start-2 col-span-1 flex justify-end items-center">
                 <Link to="/login" className="text-sm hover:text-green-300 transition duration-200">
                     LOGIN
                 </Link>
             </div>
 
             {/* Add Role Link */}
-            <div className="col-start-10 col-span-1 flex justify-end items-center">
+            <div className="col-start-3 col-span-1 flex justify-end items-center">
+                <Link to="/accountcreation" className="text-sm hover:text-green-300 transition duration-200">
+                    Creer un utilisateur
+                </Link>
+            </div>
+
+            {/* Add Role Link */}
+            <div className="col-start-4 col-span-1 flex justify-end items-center">
                 <Link to="/add-role" className="text-sm hover:text-green-300 transition duration-200">
-                    ADD ROLE
+                    Liste d'utilisateurs
+                </Link>
+            </div>
+
+            {/* Add Role Link */}
+            <div className="col-start-5 col-span-1 flex justify-end items-center">
+                <Link to="/management" className="text-sm hover:text-green-300 transition duration-200">
+                    Gestion des cours
                 </Link>
             </div>
         </div>
