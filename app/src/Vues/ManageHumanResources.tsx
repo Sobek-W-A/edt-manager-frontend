864c58bac0eac624d0c7455c9bd4fa544eff8cae
Julien Feraux <56791765+julienferaux@users.noreply.github.com>
2024-11-26 17:08:57 +0100

    update tree

--- app/src/Vues/ManageHumanResources.tsx
+++ app/src/Vues/ManageHumanResources.tsx
@@ -1,21 +1,28 @@
 import {Component} from 'react';
+import SplitPane from 'react-split-pane';
 import Tree from "../Components/CoursesManager/Tree.tsx";
 import CourseInformation from "../Components/CoursesManager/CourseInformation.tsx";
-
 class ManageHumanResources extends Component {
+    
     render() {
         return (
-            <div className="w-full h-full">
-                <div className="grid grid-cols-10">
-                    <div className="col-start-1 col-span-3 border-r-4">
-                        <Tree/>
+            <div className="flex-grow flex flex-col" style={{ height: '100%', overflow: 'hidden' }}>
+                <SplitPane
+                    split="vertical"
+                    minSize={100}
+                    defaultSize={100}
+                    pane1Style={{ display: 'flex', flexDirection: 'column', overflow: 'auto', height: '100%' }}
+                    pane2Style={{ display: 'flex', flexDirection: 'column', overflow: 'auto', height: '100%' }}
+                    resizerStyle={{ cursor: 'col-resize', background: '#ccc', width: '5px' }}
+                    style={{ height: '88.5%' }}
+                >
+                    <div className="border-r-4" style={{ height: '100%' }}>
+                        <Tree style={{ height: '100%', overflow: 'auto' }} />
                     </div>
-                    <div className="col-start-4 col-span-7 border-r-4">
-                        <CourseInformation/>
+                    <div className="border-l-4" style={{ height: '100%' }}>
+                        <CourseInformation style={{ height: '100%', overflow: 'auto' }} />
                     </div>
-
-                </div>
-
+                </SplitPane>
             </div>
         );
     }
