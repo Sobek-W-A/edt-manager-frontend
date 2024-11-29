04e18a686a4ca6d7f33f48afd08cde9ab54bbc9a
MatthieuGalante <matthieu.galante@gmail.com>
2024-11-30 00:28:42 +0100

    c'est bon

--- app/src/Components/CoursesManager/CourseInformation.tsx
+++ app/src/Components/CoursesManager/CourseInformation.tsx
@@ -31,26 +31,29 @@ const CourseInformation = forwardRef ( (props, ref) => {
     const [ueName, setUeName] = useState<string>('');
     const [courses, setCourses] = useState<Course[]>([]);
     const [isEditing, setIsEditing] = useState<boolean>(false);
+    const [idUE, setIDUE] = useState<string>('');
 
     // Exposez la méthode displayUE_By_ID via la référence
     useImperativeHandle(ref, () => ({
         displayUE_By_ID(id: string) {
-            console.log(id + " TODO");
+            console.log(id + " ON UTILISE CETTE ID");
+            setIDUE(id)
+            const fetchUEData = async () => {
+                try {
+                    const response = null //TODO appel de l'API pour tout ce qu'il faut
+                    //setUeName(response.data.name);
+                    //setCourses(response.data.courses);
+                } catch (error) {
+                    console.error('Erreur lors de la récupération des données de l\'UE:', error);
+                }
+            };
+
+            fetchUEData();
         }
     }));
 
     useEffect(() => {
-        const fetchUEData = async () => {
-            try {
-                const response = null //TODO appel de l'API pour tout ce qu'il faut
-                //setUeName(response.data.name);
-                //setCourses(response.data.courses);
-            } catch (error) {
-                console.error('Erreur lors de la récupération des données de l\'UE:', error);
-            }
-        };
 
-        fetchUEData();
     });
 
     const handleDoubleClick = () => {
@@ -69,6 +72,7 @@ const CourseInformation = forwardRef ( (props, ref) => {
     return (
         <div className="p-5">
             <div className="w-full text-center">
+
                 {isEditing ? (
                     <input
                         type="text"
