d3d54445652f085c11e0fe5a6d44bfe4f56c00e1
Boubix88 <sandygehin2@gmail.com>
2025-01-09 15:31:16 +0100

    Fix guard
    - The guard works correctly now

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -10,17 +10,18 @@ import AddAccount from "./Vues/AddAccount.tsx";
 import ManageHumanResources from "./Vues/ManageHumanResources.tsx";
 import AddProfile from "./Vues/AddProfile.tsx";
 import AssignedCoursesPage from './Vues/AssignedCoursesPage.tsx';
+import Guard from "./Components/Utils/Guard.tsx";
 
 
 // Configuration des routes
 const routes = createRoutesFromElements(
     <Route path="/" element={<Layout />}>
-        <Route path="add-role" element={<AddRole />} />
-        <Route path="accountcreation" element={<AddAccount />} />
-        <Route path="profileCreation" element={<AddProfile/>} />
-        <Route path="management" element={<ManageHumanResources />} />
+        <Route path="add-role" element={<Guard><AddRole /></Guard>} />
+        <Route path="accountcreation" element={<Guard><AddAccount /></Guard>} />
+        <Route path="profileCreation" element={<Guard><AddProfile /></Guard>} />
+        <Route path="management" element={<Guard><ManageHumanResources /></Guard>} />
         <Route path="login" element={<LoginVue />} />
-        <Route path="assigned-courses" element={<AssignedCoursesPage />} />
+        <Route path="assigned-courses" element={<Guard><AssignedCoursesPage /></Guard>} />
     </Route>
 );
 
