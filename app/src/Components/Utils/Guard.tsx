dbaba47a853d6b7d518d1fad40ad332c9ebda756
mohameddbenhamou <mohamedbenhamou.info@gmail.com>
2025-03-04 22:35:53 +0100

    Sauvegarde de mon travail sur dev

--- app/src/Components/Utils/Guard.tsx
+++ app/src/Components/Utils/Guard.tsx
@@ -1,22 +1,30 @@
 import { Navigate, useLocation } from 'react-router-dom';
-import AuthModel from '../../scripts/Models/AuthModel'; // Assurez-vous que ce modèle contient la logique d'authentification
-
 import { ReactNode } from 'react';
+import AuthModel from '../../scripts/Models/AuthModel';
 
 /**
- * Guard component that checks if the user is logged in. If not, it redirects to the login page.
- * 
- * @param children The children to render if the user is logged in.
- * @returns The children if the user is logged in, a redirection to the login page otherwise.
+ * Guard component to restrict access based on authentication and roles.
+ *
+ * @param children The content to render if access is granted.
+ * @param allowedRoles Array of roles allowed to access the route.
  */
-const Guard = ({ children }: { children: ReactNode }) => {
+const Guard = ({ children, allowedRoles }: { children: ReactNode; allowedRoles: string[] }) => {
     const location = useLocation();
 
-    return AuthModel.isLoggedIn() ? (
-        children
-    ) : (
-        <Navigate to="/login" state={{ from: location }} />
-    );
+    // Redirect to login if not authenticated
+    if (!AuthModel.isLoggedIn()) {
+        return <Navigate to="/login" state={{ from: location }} />;
+    }
+
+    const userRole = AuthModel.getRole();
+
+    // Redirect to unauthorized page if role is not allowed
+    if (!allowedRoles.includes(userRole)) {
+        return <Navigate to="/unauthorized" />;
+    }
+
+    // Render children if all checks pass
+    return <>{children}</>;
 };
 
 export default Guard;
